75957922d9e6bafc7a89dd9e03100f89cc19b731
LightCode5794 <90496996+LightCode5794@users.noreply.github.com>
2023-10-05 17:11:33 +0700

    update base backend

---
+++ FoodDelivery.Application/Features/Users/Commands/CreateUser/CreateUserCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using FoodDelivery.Application.Interfaces.Repositories;
+using FoodDelivery.Domain.Entities;
+using FoodDelivery.Application.Common.Mappings;
+
+namespace FoodDelivery.Application.Features.Users.Commands.CreateUser
+{
+    internal class CreateUserCommand
+    {
+    }
+}
